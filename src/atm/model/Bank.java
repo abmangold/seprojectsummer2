7cb26dccb184113457e6c4d13285c3ef46c49854
Walter Garciaguirre <waltergarciaguirre@gmail.com>
2019-07-20 10:54:38 -0500

    Added Bank Class

---
+++ src/atm/model/Bank.java
@@ -0,0 +1,23 @@
+package atm.model;
+import java.util.*;
+
+public class Bank {
+	
+	//ArrayList for the users of the bank "UserAccounts" and for the different types
+	//of accounts for each of those users "BankAccounts"
+	private ArrayList<Integer> UserAccounts = new ArrayList<Integer>();
+	private ArrayList<Integer> BankAccounts = new ArrayList<Integer>();
+	
+	//Gets users account when signing in
+	public int getUserAccount() {
+		
+		return(0);
+	}
+	
+	//Gets users different accounts within the bank
+	public int getUserBankAccounts() {
+		
+		return(0);
+	}
+
+}
