abf77ed200fd7987b169f79b9abd520353ae5a71
christopher-carson <Xzoner@gmail.com>
2019-07-23 08:02:29 -0500

    Add Session class

--- src/atm/model/BankAccount.java
+++ src/atm/model/BankAccount.java
@@ -1,14 +1,16 @@
 package atm.model;
 
+import java.math.BigDecimal;
+
 public class BankAccount extends AbstractModel{
-	private double Balance;
+	private BigDecimal Balance;
 	private String Name;
 	private String ID;
 	private String PIN;
 	public BankAccount(String name, 
 					   String ID, 
 					   String PIN, 
-					   double balance) {
+					   BigDecimal balance) {
 		this.setName(Name);
 		this.setID(ID);
 		this.setPIN(PIN);
@@ -32,26 +34,26 @@ public class BankAccount extends AbstractModel{
 	private void setName(String name) {
 		this.Name = name;
 	}
-	public double getBalance() {
+	public BigDecimal getBalance() {
 		return Balance;
 	}
-	private void setBalance(double balance) {
+	private void setBalance(BigDecimal balance) {
 		this.Balance = balance;
 	}
 	
-	public double withdraw(double amount) throws Exception {
-		double newBalance = Balance;
-		newBalance -= amount;
-		if (newBalance < 0) throw new Exception("OverDraw");
+	public BigDecimal withdraw(BigDecimal amount) throws Exception {
+		BigDecimal newBalance = Balance.add(BigDecimal.ZERO);
+		newBalance.subtract(amount);
+		if (newBalance.signum() < 0) throw new Exception("OverDraw");
 		// Add Model Event
 		Balance = newBalance;
 		// Notify Changed
 		return Balance;
 	}
 	
-	public double deposit(double amount) {
-		double newBalance = Balance;
-		newBalance += amount;
+	public BigDecimal deposit(BigDecimal amount) {
+		BigDecimal newBalance = Balance.add(BigDecimal.ZERO);
+		newBalance.add(amount);
 		// Add Model Event
 		Balance = newBalance;
 		// Notify Changed
