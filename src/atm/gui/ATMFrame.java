1b64ee6f7ae72c8da382f25f40d0455d2a56c561
abmangold <31642971+abmangold@users.noreply.github.com>
2019-07-29 00:45:27 -0500

    Update ATMFrame.java
    Numbers display in field

--- src/atm/gui/ATMFrame.java
+++ src/atm/gui/ATMFrame.java
@@ -20,7 +20,7 @@ import java.awt.Color;
 public class ATMFrame {
 
 	private JFrame frame;
-	private JTextField textField;
+	private JTextField NumberDisplay;
 
 	/**
 	 * Launch the application.
@@ -62,53 +62,136 @@ public class ATMFrame {
 		KeyPad.setLayout(new GridLayout(4, 3, 10, 10));
 		
 		JButton btnNewButton = new JButton("7");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String enterNumber = NumberDisplay.getText() + "7";
+				NumberDisplay.setText(enterNumber);
+			}
+		});
 		
 		btnNewButton.setFont(new Font("Trebuchet MS", Font.PLAIN, 24));
 		KeyPad.add(btnNewButton);
 		JButton btnNewButton_1 = new JButton("8");
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				String enterNumber = NumberDisplay.getText() + "8";
+				NumberDisplay.setText(enterNumber);
 			}
 		});
 		btnNewButton_1.setFont(new Font("Trebuchet MS", Font.PLAIN, 24));
 		KeyPad.add(btnNewButton_1);
 		JButton btnNewButton_2 = new JButton("9");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String enterNumber = NumberDisplay.getText() + "9";
+				NumberDisplay.setText(enterNumber);
+			}
+		});
 		btnNewButton_2.setFont(new Font("Trebuchet MS", Font.PLAIN, 24));
 		KeyPad.add(btnNewButton_2);
 		JButton btnNewButton_3 = new JButton("4");
+		btnNewButton_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String enterNumber = NumberDisplay.getText() + "4";
+				NumberDisplay.setText(enterNumber);
+			}
+		});
 		btnNewButton_3.setFont(new Font("Trebuchet MS", Font.PLAIN, 24));
 		KeyPad.add(btnNewButton_3);
 		JButton btnNewButton_4 = new JButton("5");
+		btnNewButton_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String enterNumber = NumberDisplay.getText() + "5";
+				NumberDisplay.setText(enterNumber);
+			}
+		});
 		btnNewButton_4.setFont(new Font("Trebuchet MS", Font.PLAIN, 24));
 		KeyPad.add(btnNewButton_4);
 		JButton btnNewButton_5 = new JButton("6");
+		btnNewButton_5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String enterNumber = NumberDisplay.getText() + "6";
+				NumberDisplay.setText(enterNumber);
+			}
+		});
 		btnNewButton_5.setFont(new Font("Trebuchet MS", Font.PLAIN, 24));
 		KeyPad.add(btnNewButton_5);
 		JButton btnNewButton_6 = new JButton("1");
+		btnNewButton_6.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String enterNumber = NumberDisplay.getText() + "1";
+				NumberDisplay.setText(enterNumber);
+			}
+		});
 		btnNewButton_6.setFont(new Font("Trebuchet MS", Font.PLAIN, 24));
 		KeyPad.add(btnNewButton_6);
 		JButton btnNewButton_7 = new JButton("2");
+		btnNewButton_7.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String enterNumber = NumberDisplay.getText() + "2";
+				NumberDisplay.setText(enterNumber);
+			}
+		});
 		btnNewButton_7.setFont(new Font("Trebuchet MS", Font.PLAIN, 24));
 		KeyPad.add(btnNewButton_7);
 		JButton btnNewButton_8 = new JButton("3");
+		btnNewButton_8.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String enterNumber = NumberDisplay.getText() + "3";
+				NumberDisplay.setText(enterNumber);
+			}
+		});
 		btnNewButton_8.setFont(new Font("Trebuchet MS", Font.PLAIN, 24));
 		KeyPad.add(btnNewButton_8);
 		JButton btnNewButton_9 = new JButton("0");
-		btnNewButton_9.setFont(new Font("Times New Roman", Font.PLAIN, 24));
+		btnNewButton_9.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String enterNumber = NumberDisplay.getText() + "0";
+				NumberDisplay.setText(enterNumber);
+			}
+		});
+		btnNewButton_9.setFont(new Font("Trebuchet MS", Font.PLAIN, 24));
 		KeyPad.add(btnNewButton_9);
 		JButton btnNewButton_10 = new JButton(".");
+		btnNewButton_10.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String enterNumber = NumberDisplay.getText() + ".";
+				NumberDisplay.setText(enterNumber);
+			}
+		});
 		btnNewButton_10.setFont(new Font("Trebuchet MS", Font.PLAIN, 24));
 		KeyPad.add(btnNewButton_10);
 		
 		JButton clearButton = new JButton("CE");
+		clearButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				NumberDisplay.setText("");
+			}
+		});
 		clearButton.setFont(new Font("Trebuchet MS", Font.PLAIN, 24));
 		KeyPad.add(clearButton);
 		
-		textField = new JTextField();
-		textField.setBounds(85, 281, 285, 43);
-		frame.getContentPane().add(textField);
-		textField.setColumns(10);
+		NumberDisplay = new JTextField();
+		NumberDisplay.setBounds(85, 281, 285, 43);
+		frame.getContentPane().add(NumberDisplay);
+		NumberDisplay.setColumns(10);
 		
+		/*
+		public void addButton(final String label) {
+			class DigitButtonListener implements ActionListener {
+				public void actionPerformed(ActionEvent e) {
+					if (label.equals(".") && NumberDisplay.getText().indexOf(".") != -1) {
+						return;
+					}
+					NumberDisplay.setText(NumberDisplay.getText() + label);
+				}				
+			}
+			JButton button = new JButton(label);
+			KeyPad.add(button);
+			ActionListener listener = new DigitButtonListener();
+			button.addActionListener(listener);			
+		}
+		*/
 		JPanel SelectionPad = new JPanel();
 		SelectionPad.setBackground(SystemColor.activeCaption);
 		SelectionPad.setBounds(428, 335, 110, 329);
