419856d468706cc6ee5e075172afb6d37608c1e2
christopher-carson <Xzoner@gmail.com>
2019-08-05 09:04:49 -0500

    Fix up PinAuthenticator and tests

--- src/atm/controller/PinAuthenticator.java
+++ src/atm/controller/PinAuthenticator.java
@@ -2,41 +2,30 @@ package atm.controller;
 
 import java.math.BigDecimal;
 
+import atm.model.AccountLockException;
 import atm.model.BankAccount;
 
 public class PinAuthenticator {
-	BankAccount ba = new BankAccount("Checking", "1234", "1111", new BigDecimal("200.00")); // Only here for testing purposes, otherwise the object would have existing parameters already.
-	private String pin;
-	//private String test = ba.getPIN();
-	private int attempts = 3;
-	private Boolean lock = false;
+	BankAccount ba;
+	private int attempts;
 	
-	public PinAuthenticator(String PIN) {
-		this.pin = PIN;
+	public PinAuthenticator(BankAccount ba) {
+		this.ba = ba;
+		this.attempts = 3;
 	}
+		
 	
-	public Boolean checkLockStatus() {
-		return lock;
-	}
-	
-	
-	public void checkPin() {
-		do {
-			try {
-				if(pin.equals(ba.getPIN())) {
-					System.out.println("Welcome"); // Msg for clearance testing 
-					break;
-				}
+	public boolean checkPin(String PINtoValidate) throws AccountLockException {
+			if(PINtoValidate.equals(ba.getPIN())) {
+				return true;
 			}
-			catch (Exception e) {
-				System.out.println("Incorrect PIN. Try again."); // Msg for fail testing
-			}
+
 			attempts--;
-		}while(attempts > 0);
 		
 		if(attempts == 0) {
-			System.out.println("Exceeded attempts, locking now.");
-			lock = true;
+			ba.setAccountLock(true);
+			throw new AccountLockException();
 		}
+		return false;
 	}
 }
