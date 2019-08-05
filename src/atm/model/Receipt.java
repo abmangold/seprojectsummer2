ad74641ade0dc97eda922559fab15908fabefef7
christopher-carson <Xzoner@gmail.com>
2019-08-05 10:35:57 -0500

    Update Receipt Class
    Simplify receipt line adding

--- src/atm/model/Receipt.java
+++ src/atm/model/Receipt.java
@@ -15,43 +15,90 @@ public class Receipt {
 
 	private ArrayList<String> ReceiptLines = new ArrayList<String>();
 	
+	
 	/**
-	 * Processes a transaction event on an Account and adds it the ReceiptLines list.
-	 * @see atm.model.TransactionEvent
-	 * @param account BankAccount the transaction is being performed on.
-	 * @param event The type of TransactionEvent.
-	 * @param amount The amount applied to the account based on event.
+	 * Default constructor for Receipt class
 	 */
-	public void ProcessEvent(BankAccount account, TransactionEvent event, BigDecimal amount) {
-		String receiptLine = "(" + account.toString() + ")";
-		switch(event) {
-		case Withdraw:
-			receiptLine = "$" + amount + " withdrawn from " + receiptLine;
-			break;
-		case Deposit:
-			receiptLine = "$" + amount + " deposit from " + receiptLine;
-			break;
-		case Transfer_Withdraw:
-			receiptLine = "$" + amount + " transfered from " + receiptLine;
-			break;
-		case Transfer_Deposit:
-			receiptLine = "$" + amount + " transfered to " + receiptLine;
-			break;
-		case Balance:
-			receiptLine = "$"+ account.getBalance() +"Balance "+ receiptLine;
-			break;
-		}
-		ReceiptLines.add(receiptLine);
+	public Receipt() {
+		ReceiptLines.add("#########################################");
+		ReceiptLines.add("######         ATM RECEIPT        #######");
+		ReceiptLines.add("#########################################");
+		ReceiptLines.add("######  Scrooge McDuck Bank Inc.  #######");
+		ReceiptLines.add("#########################################");
+		ReceiptLines.add("");
 	}
 	
+	/**
+	 * Adds withdraw line items to the receipt.
+	 * @param ba BankAccount withdrawn from.
+	 * @param amount Amount that was withdrawn.
+	 */
+	public void addWithdraw(BankAccount ba, BigDecimal amount) {
+		ReceiptLines.add("Withdraw from " + ba.toString() + "     -$" + amount.toString());
+		ReceiptLines.add("");
+		addBalance(ba);
+	}
 	
+	/**
+	 * Adds deposit line items to the receipt.
+	 * @param ba BankAccount that was deposited into.
+	 * @param amount Amount that was deposited.
+	 */
+	public void addDeposit(BankAccount ba, BigDecimal amount) {
+		ReceiptLines.add("Deposit into " + ba.toString() + "     +$" + amount.toString());
+		ReceiptLines.add("");
+		addBalance(ba);
+	}
+	
+	/**
+	 * Adds transfer line items to the receipt.
+	 * @param ba1 BankAccount transferred from.
+	 * @param ba2 BankAccount transferred to.
+	 * @param amount Amount that was transferred.
+	 */
+	public void addTransfer(BankAccount ba1, BankAccount ba2, BigDecimal amount) {
+		ReceiptLines.add("Transfer from [" + ba1.toString() + "]     -$" + amount.toString());
+		ReceiptLines.add("Transfer into [" + ba2.toString() + "]     +$" + amount.toString());
+		ReceiptLines.add("");
+		addBalance(ba1);
+		addBalance(ba2);
+	}
+	
+	/**
+	 * Adds a balance line to the receipt.
+	 * @param ba BankAccount to get balance. 
+	 */
+	public void addBalance(BankAccount ba) {
+		ReceiptLines.add("[" + ba.toString() +"] Available Balance      $" + ba.getBalance().toString());
+	}
+	
+	/**
+	 * Generates a bank account balance Receipt.
+	 * @param ba BankAccount to show balance of.
+	 * @return Receipt showing the balance of the Bank Account
+	 */
+	public static Receipt getBalanceReceipt(BankAccount ba) {
+		Receipt rec = new Receipt();
+		rec.addBalance(ba);
+		return rec;
+	}
+
 	/**
 	 * Retrieves the ReceiptLines.
 	 * @return ArrayList of ReceiptLines of the Receipt.
 	 */
-	public ArrayList<String> getReceipt()
-	{
+	public ArrayList<String> getReceiptLines() {
 		return ReceiptLines;
 	}
 	
+	
+	/**
+	 * Prints the receipt to System.out
+	 */
+	public void PrintReceipt()
+	{
+		for(String line : ReceiptLines) {
+			System.out.println(line);
+		}
+	}
 }
